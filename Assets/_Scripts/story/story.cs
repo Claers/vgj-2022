57e3ff3bd5eaa79342ea769953fa992a7d1c2342
Papy-Caelioras <caelioras@gmail.com>
2022-11-19 21:23:12 +0400

    samedi soir

--- Assets/_Scripts/story/story.cs
+++ Assets/_Scripts/story/story.cs
@@ -2,9 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using DG.Tweening;
 
 public class story : MonoBehaviour
 {
+    public List<Vector3> _positionCam;
+    public int _step = 0;
+
     // Start is called before the first frame update
     void OnEnable()
     {
@@ -16,9 +20,14 @@ public class story : MonoBehaviour
         Main.Instance.Input.MenuActions.Next.started -= MoveCam;
     }
 
+    void Start()
+    {
+    }
+
     public void MoveCam(InputAction.CallbackContext context)
     {
-
+        Main.Instance.CameraManager.mainCamera.transform.DOMove(_positionCam[_step], 2f);
+        _step++;
     }
 
     // Update is called once per frame
